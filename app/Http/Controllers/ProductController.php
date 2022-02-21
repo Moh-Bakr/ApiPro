4ebfd9acf0ac675d3c7e0fe95343adbac5501580
MBakr <moh.bakr27@gmail.com>
2022-02-21 19:27:47 +0200

    some cleanups

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,46 +8,26 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return Product::get()->all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StorePostRequest $request)
     {
         $request->validated();
         return Product::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         return Product::find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         $product = Product::find($id);
@@ -56,23 +36,13 @@ class ProductController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return int
-     */
+
     public function destroy($id)
     {
         return Product::destroy($id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param string $name
-     * @return int
-     */
+
     public function search($name)
     {
         return Product::where('name', 'like', '%' . $name . '%')->get();
